6fac02985d16000ba325193127becddb26383dd6
ChaimaSahli <st.chaima25@gmail.com>
2024-02-07 16:30:35 +0000

    course model added

---
+++ Models/Course.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose')
+
+const courseSchema=new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+      category: {
+        type: String,
+        required: true
+      },
+      unit:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Unit',
+    }
+
+
+})
+
+module.exports= mongoose.model('Course', courseSchema )
