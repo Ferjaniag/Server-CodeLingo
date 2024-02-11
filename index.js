75fe1abab667a5b6461a398bd3547ec7261a0bfe
ChaimaSahli <st.chaima25@gmail.com>
2024-02-11 18:22:34 +0000

    corrected the cors

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors"); // hope it works now
 
+require("dotenv").config();
+require("./config/mongoose");
 
-
-
-require("dotenv").config()
-require("./config/mongoose")
-
-app.use(bodyParser.json()) 
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
 // routes
 const course_routes = require("./Routes/CourseRoutes");
-const unit_routes = require("./Routes/UnitRoute")
+const unit_routes = require("./Routes/UnitRoute");
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
-
-
 
 app.listen(process.env.PORT, () => {
   console.log("App is listening on port", process.env.PORT);
@@ -28,4 +24,4 @@ app.listen(process.env.PORT, () => {
 
 //app use
 app.use("/", course_routes);
-app.use("/",unit_routes);
+app.use("/", unit_routes);
