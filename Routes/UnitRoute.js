a0299befdc220a67f4dad57d333f99797489be85
ferjania <ferjeniag@gmail.com>
2024-02-11 21:46:13 +0100

    update route commit

--- Routes/UnitRoute.js
+++ Routes/UnitRoute.js
@@ -5,7 +5,7 @@ const unitController = require('../Controllers/UnitController');
 router.post('/add_unit', unitController.addUnit);
 router.get('/get_units',unitController.getUnitsWithCourseNames);
 router.delete('/delete_unit/:unitId', unitController.deleteUnit);
-router.put('/update/:unitId', unitController.updateUnit);
+router.put('/update_unit/:unitId', unitController.updateUnit);
 router.post('/add_lesson/:unitId', unitController.addLessonToUnit);
 router.post('/add_quiz/:unitId', unitController.addQuizToUnit);
 
