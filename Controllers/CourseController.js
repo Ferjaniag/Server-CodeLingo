644f67ae346c2795438607bb921022ba68921fb7
ChaimaSahli <st.chaima25@gmail.com>
2024-02-14 20:28:53 +0000

    update course and get course details

--- Controllers/CourseController.js
+++ Controllers/CourseController.js
@@ -1,42 +1,73 @@
-const Course = require('../Models/Course');
+const Course = require("../Models/Course");
 
-exports.add_course = async (req, res) =>{
-    try{
-        const {title,description,category,unit} = req.body;
-        const data = { title, description, category,unit };
-        const new_course = await Course.create(data);
-        res.status(200).send(new_course);
-    }catch(err){
-        res.status(500).send({message:err.message});
-    }
-}
+exports.add_course = async (req, res) => {
+  try {
+    const { title, description, category, unit } = req.body;
+    const data = { title, description, category, unit };
+    const new_course = await Course.create(data);
+    res.status(200).send(new_course);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
-exports.get_all_courses = async (req, res) =>{
-    try{
-        await Course.find({})
-        .then((courses) => {
-            if(courses){
-                res.status(200).send(courses);
-            }else{
-                res.status(404).send("Courses not found");
-            }
-        })
-    }catch(err){
-        res.status(500).send({err:err.message});
-    }
-}
-
+exports.get_all_courses = async (req, res) => {
+  try {
+    await Course.find({}).then((courses) => {
+      if (courses) {
+        res.status(200).send(courses);
+      } else {
+        res.status(404).send("Courses not found");
+      }
+    });
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
 
 exports.delete_course = async (req, res) => {
-    try {
-        const result = await Course.findByIdAndDelete({_id: req.params.courseID});
-        
-        if (result) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).json({ message: "Course not found" });
-        }
-    } catch (err) {
-        res.status(400).json(err);
+  try {
+    const result = await Course.findByIdAndDelete({ _id: req.params.courseID });
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: "Course not found" });
     }
-}
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+//! function to update a course
+
+module.exports.update_course = async (req, res) => {
+  try {
+    const UpdatedCourse = await Course.findByIdAndUpdate(
+      req.params.courseID,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!UpdatedCourse) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    res.status(200).json(UpdatedCourse);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+//! get one course
+module.exports.get_one_course = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.courseID);
+    if (!course) {
+      return res.status(404).json({ message: "course not found" });
+    }
+
+    res.status(200).json(course);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
--- Routes/CourseRoutes.js
+++ Routes/CourseRoutes.js
@@ -4,6 +4,8 @@ const router = Router();
 
 router.post('/add_course', course_controllers.add_course);
 router.get('/get_all_courses', course_controllers.get_all_courses);
+router.get('/get_course/:courseID', course_controllers.get_one_course);
 router.delete('/delete_course/:courseID',course_controllers.delete_course);
+router.patch('/update_course/:courseID',course_controllers.update_course);
 
 module.exports = router;
